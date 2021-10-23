06721c211a802811368ed06eacaa3cbc26f0c82d
XboxBedrock <68715625+XboxBedrock@users.noreply.github.com>
2021-10-23 16:44:37 -0700

    feex

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,12 @@
 declare module 'minecraft-block-info' {
-    export async function getBlockImageID(ids: any[]): Promise<string>;
-    export async function getBlockImageObject(objects: any[]): Promise<string>;
+    export function getBlockImageID(ids: any[]): Promise<string>;
+    export function getBlockImageObject(objects: any[]): Promise<string>;
     export function isValidBlockID(id: string): boolean;
     export function isValidBlockName(name: string): boolean;
     export function isValidBlockDisplayName(displayName: string): boolean;
-    export async function getBlockInfoFromIDs(ids: any[]): Promise<any[]>;
-    export async function getBlockInfoFromNames(names: any[]): Promise<any[]>;
-    export async function getBlockInfoFromDisplayNames(displayNames: any[]):Promise<any[]>;
-    export async function searchAbsolute(queryList: any[]): Promise<any[]>;
-    export async function search(query: string): Promise<any[]>;
+    export function getBlockInfoFromIDs(ids: any[]): Promise<any[]>;
+    export function getBlockInfoFromNames(names: any[]): Promise<any[]>;
+    export function getBlockInfoFromDisplayNames(displayNames: any[]):Promise<any[]>;
+    export function searchAbsolute(queryList: any[]): Promise<any[]>;
+    export function search(query: string): Promise<any[]>;
 }
