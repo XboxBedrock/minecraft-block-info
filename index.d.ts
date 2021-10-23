37a088ae6604dfda9551f97bdd7f16c0edb62657
XboxBedrock <68715625+XboxBedrock@users.noreply.github.com>
2021-10-23 16:41:23 -0700

    fix

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,12 @@
 declare module 'minecraft-block-info' {
-    export async function getBlockImageID(ids: any[]): string;
-    export async function getBlockImageObject(objects: any[]): string;
+    export async function getBlockImageID(ids: any[]): Promise<string>;
+    export async function getBlockImageObject(objects: any[]): Promise<string>;
     export function isValidBlockID(id: string): boolean;
     export function isValidBlockName(name: string): boolean;
     export function isValidBlockDisplayName(displayName: string): boolean;
-    export async function getBlockInfoFromIDs(ids: any[]): any[];
-    export async function getBlockInfoFromNames(names: any[]): any[];
-    export async function getBlockInfoFromDisplayNames(displayNames: any[]): any[];
-    export async function searchAbsolute(queryList: any[]): any[];
-    export async function search(query: string): any[];
+    export async function getBlockInfoFromIDs(ids: any[]): Promise<any[]>;
+    export async function getBlockInfoFromNames(names: any[]): Promise<any[]>;
+    export async function getBlockInfoFromDisplayNames(displayNames: any[]):Promise<any[]>;
+    export async function searchAbsolute(queryList: any[]): Promise<any[]>;
+    export async function search(query: string): Promise<any[]>;
 }
